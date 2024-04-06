075dea7f15d3344bb34d4f2b43a01246b7db7d70
墨綠BG <s5460703@gmail.com>
2024-04-06 23:53:39 +0800

    🔧 chore：配置 next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
