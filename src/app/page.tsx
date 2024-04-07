74dc656358c6aec35ef78ee9776d8d11f45d714f
墨綠BG <s5460703@gmail.com>
2024-04-07 14:50:07 +0800

    ✨ feat：新增搜索框动画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { NextPage } from 'next';
 import Image from 'next/image';
+
+import { motion } from 'framer-motion';
 
 const HomePage: NextPage = () => {
   return (
@@ -12,12 +16,15 @@ const HomePage: NextPage = () => {
           height={626}
           className="relative top-1 h-full w-full select-none"
         />
-        <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-          <input className="h-[5vh] w-[35%] rounded-l-lg border-2 border-[#841710] text-lg outline-none px-3" />
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          className="absolute left-0 top-0 flex h-full w-full items-center justify-center"
+        >
+          <input className="h-[5vh] w-[35%] rounded-l-lg border-2 border-[#841710] px-3 text-lg outline-none" />
           <button className="h-[5vh] w-[8%] rounded-r-lg bg-[#841710] text-white">
             检索
           </button>
-        </div>
+        </motion.div>
       </div>
     </>
   );
