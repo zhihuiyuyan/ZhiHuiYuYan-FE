ad56bd41fc435580da9e627eae3e71b352a5d9d1
墨綠BG <s5460703@gmail.com>
2024-04-08 20:17:34 +0800

    🌈 style：调整组件 ts type

--- src/common/components/elements/Breakline.tsx
+++ src/common/components/elements/Breakline.tsx
@@ -3,7 +3,7 @@ type BreaklineProps = {
   [propName: string]: string | undefined;
 };
 
-const Breakline = ({ className = '', ...others }: BreaklineProps) => {
+const Breakline: React.FC<BreaklineProps> = ({ className = '', ...others }) => {
   return (
     <div
       className={`my-4 border-t border-gray-300 dark:border-neutral-700 ${className}`}
