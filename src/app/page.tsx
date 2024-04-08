09baadec3bc6de82a51a704bea779ee141177e98
墨綠BG <s5460703@gmail.com>
2024-04-09 02:44:30 +0800

    ✨ feat：新增文章列表

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,60 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import { useState } from 'react';
 
-const TAB_ITEMS = [
-  { id: 1, value: '研究热点' },
-  { id: 2, value: '时事新闻' },
-];
+import Breakline from '@/common/components/elements/Breakline';
+
+type ArticleItem = {
+  id: number;
+  title: string;
+  content: string;
+  timestamp: string;
+  readings: number;
+};
 
 interface TabFocusProps {
   isFocusded: boolean;
   children: React.ReactNode;
 }
+
+interface ArticleItemProps {
+  item: ArticleItem;
+}
+
+const TAB_ITEMS = [
+  { id: 1, value: '研究热点' },
+  { id: 2, value: '时事新闻' },
+];
+
+const ARTICLE_ITEMS: ArticleItem[] = [
+  {
+    id: 1,
+    title: '标题',
+    content: '内容',
+    timestamp: '2024-03-30',
+    readings: 999,
+  },
+  {
+    id: 2,
+    title: '标题',
+    content: '内容',
+    timestamp: '2024-03-30',
+    readings: 999,
+  },
+  {
+    id: 3,
+    title: '标题',
+    content: '内容',
+    timestamp: '2024-03-30',
+    readings: 999,
+  },
+  {
+    id: 4,
+    title: '标题',
+    content: '内容',
+    timestamp: '2024-03-30',
+    readings: 999,
+  },
+];
 
 const TabFocus: React.FC<TabFocusProps> = ({ isFocusded, children }) => {
   return (
@@ -40,6 +85,50 @@ const TabFocus: React.FC<TabFocusProps> = ({ isFocusded, children }) => {
         />
       )}
     </motion.div>
+  );
+};
+
+const ArticleItem: React.FC<ArticleItemProps> = ({ item }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <>
+      <div
+        className="flex h-[18vh] w-full cursor-pointer px-[7%] pt-[2vh]"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="relative flex flex-[4] flex-col">
+          <h3 className={`text-xl ${isHovered && 'underline'}`}>
+            {item.title}
+          </h3>
+          <p className="pt-1 text-[#939393]">{item.content}</p>
+          <span className="absolute bottom-0 flex text-sm text-[#9F9F9F]">
+            <p>发布时间：{item.timestamp}</p>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <p>阅读量：{item.readings}</p>
+          </span>
+        </div>
+        <div className="flex-1 overflow-hidden">
+          <Image
+            src="/images/main/article-image.png"
+            alt="main-bg"
+            width={124}
+            height={83}
+            className={`h-full w-full select-none ${isHovered && 'blur-lg' && 'scale-110'}`}
+          />
+        </div>
+      </div>
+      <Breakline className="w-[90%]" />
+    </>
   );
 };
 
@@ -74,7 +163,7 @@ const HomePage: NextPage = () => {
           height={12}
           className="w-full select-none"
         />
-        <div className="flex h-[70vh] w-full px-[2%] pb-10">
+        <div className="flex h-auto w-full px-[2%] pb-10">
           <div className="flex flex-[2.5] flex-col">
             <div className="flex h-[8vh] items-center">
               {TAB_ITEMS.map((item) => (
@@ -90,8 +179,10 @@ const HomePage: NextPage = () => {
                 </TabFocus>
               ))}
             </div>
-            <div className="h-full rounded-xl border border-[#BBBBBB]">
-              
+            <div className="flex h-full flex-col items-center rounded-xl border border-[#BBBBBB]">
+              {ARTICLE_ITEMS.map((item) => (
+                <ArticleItem key={item.id} item={item} />
+              ))}
             </div>
           </div>
           <div className="flex-1"></div>
---
+++ src/common/components/elements/Breakline.tsx
@@ -0,0 +1,16 @@
+type BreaklineProps = {
+  className?: string;
+  [propName: string]: string | undefined;
+};
+
+const Breakline: React.FC<BreaklineProps> = ({ className = '', ...others }) => {
+  return (
+    <div
+      className={`my-4 border-t border- border-gray-300 dark:border-neutral-700 ${className}`}
+      data-testid="breakline"
+      {...others}
+    ></div>
+  );
+};
+
+export default Breakline;
