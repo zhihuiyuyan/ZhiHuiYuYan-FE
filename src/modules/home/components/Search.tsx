317c1c768fbc2f63fd241678fc1040ab20e5ef4f
墨綠BG <s5460703@gmail.com>
2024-04-12 22:29:43 +0800

    🐞 fix：修复搜索框比例

--- src/modules/home/components/Search.tsx
+++ src/modules/home/components/Search.tsx
@@ -7,14 +7,17 @@ const Search: React.FC = () => {
       <Image
         src="/images/main/main-bg.png"
         alt="main-bg"
-        width={1015}
-        height={626}
+        width={7864}
+        height={1549}
         className="relative top-1 -z-10 h-full w-full select-none"
       />
       <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-        <motion.div whileTap={{ scale: 0.9 }} className="flex h-[5vh] w-[43%] min-w-[220px]">
-          <input className="flex-[3] rounded-l-lg border-2 border-[#841710] px-[2%] text-[1.8vh] outline-none" />
-          <button className="flex-1 rounded-r-lg bg-[#841710] text-white text-[1.8vh] px-[2%]">
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          className="flex h-[5vh] w-[43%] min-w-[220px]"
+        >
+          <input className="flex-[3] rounded-l-lg border-2 border-[#841710] px-[2%] text-[1.8vh] outline-none lg:flex-[4]" />
+          <button className="flex-1 rounded-r-lg bg-[#841710] px-[2%] text-[1.8vh] text-white">
             检索
           </button>
         </motion.div>
