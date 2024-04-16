08e16ef8ccc8e2b04a6704dfeb16f0f417d0184d
墨綠BG <s5460703@gmail.com>
2024-04-16 23:15:42 +0800

    🌈 style：UI 做响应式设计

--- src/modules/profile/components/Profile.tsx
+++ src/modules/profile/components/Profile.tsx
@@ -10,24 +10,24 @@ const Profile = () => {
         <div className="relative flex h-[88vh] w-[60%] min-w-[50vh] flex-col items-center rounded-[3vh] bg-white">
           <div className="relative flex h-[14vh] w-full items-center">
             <div className="relative left-[7.5%] top-[1vh] h-[9vh] w-[9vh] rounded-full bg-[#E7E7E7]"></div>
-            <p className="absolute left-[18.5%] top-[4vh] text-[2vh] font-semibold">
+            <p className="absolute left-[30%] top-[4vh] text-[2vh] font-semibold md:left-[25%] lg:left-[22%] xl:left-[18.5%]">
               姓名
             </p>
-            <div className="absolute left-[18.5%] top-[8vh] flex items-center gap-[2vh] text-[1.5vh] text-[#0B489B]">
+            <div className="absolute left-[30%] top-[8vh] flex items-center gap-[2vh] text-[1.5vh] text-[#0B489B] md:left-[25%] lg:left-[22%] xl:left-[18.5%]">
               <p>性别：女</p>
               <BreaklineDashed className="h-[2vh] border-r-2 border-[#B9B9B9]" />
               <p>机构：</p>
             </div>
-            <div className="absolute right-[10%] flex gap-[3vh]">
+            <div className="absolute right-[10%] flex flex-col gap-[1vh] md:flex-row lg:gap-[3vh]">
               <motion.button
                 whileTap={{ scale: 0.9 }}
-                className="h-[5vh] w-[12vh] rounded-[1vh] bg-[#841710] text-[1.7vh] text-white"
+                className="h-[4vh] w-[10vh] rounded-[1vh] bg-[#841710] text-[1.7vh] text-white lg:h-[5vh] lg:w-[12vh]"
               >
                 我的关注
               </motion.button>
               <motion.button
                 whileTap={{ scale: 0.9 }}
-                className="h-[5vh] w-[12vh] rounded-[1vh] bg-[#841710] text-[1.7vh] text-white"
+                className="h-[4vh] w-[10vh] rounded-[1vh] bg-[#841710] text-[1.7vh] text-white lg:h-[5vh] lg:w-[12vh]"
               >
                 浏览历史
               </motion.button>
