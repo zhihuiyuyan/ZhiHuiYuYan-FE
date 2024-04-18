8bc1359672b21baf2f4e521fd1f3c16d5ec91c51
墨綠BG <s5460703@gmail.com>
2024-04-18 16:59:44 +0800

    ✨ feat：新增 Tabs 组件

--- src/modules/profile/components/Follow.tsx
+++ src/modules/profile/components/Follow.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import { useState } from 'react';
 import { IoHeart, IoHeartOutline } from 'react-icons/io5';
 
 import {
@@ -10,11 +8,7 @@ import {
   AvatarImage,
 } from '@/common/components/elements/Avatar';
 import Breakline from '@/common/components/elements/BreaklineDashed';
-
-const TAB_ITEMS = [
-  { id: 1, content: '关注学者' },
-  { id: 2, content: '论文收藏' },
-];
+import { Tabs } from '@/modules/profile/components/Tabs';
 
 const SCHOLAR_ITEMS = [
   {
@@ -44,63 +38,53 @@ const SCHOLAR_ITEMS = [
 ];
 
 const Follow = () => {
-  const [tabClicked, setTabClicked] = useState(1);
-
-  return (
-    <div className="relative flex w-full flex-col items-center">
-      <div className="flex h-[4vh] w-[80%] items-center">
-        {TAB_ITEMS.map((item) => (
-          <motion.p
-            whileTap={{ scale: 1.2 }}
-            whileHover={{ scale: 1.1 }}
-            key={item.id}
-            className={`ml-[6vh] flex h-full cursor-pointer items-center justify-center px-[3vh] text-[1.85vh] ${tabClicked === item.id ? 'rounded-t-[1vh] bg-[#F3F3F3] font-semibold text-[#0B489B]' : 'text-[#707070]'}`}
-            onClick={() => setTabClicked(item.id)}
-          >
-            {item.content}
-          </motion.p>
-        ))}
-      </div>
-      <div className="flex h-[63vh] w-[80%] flex-col items-center rounded-[3vh] bg-[#F3F3F3] px-[3vh] py-[2vh]">
-        {tabClicked === 1 && (
-          <>
-            {SCHOLAR_ITEMS.map((item) => (
-              <div
-                key={item.id}
-                className="relative flex w-full flex-col items-center"
-              >
-                <div className="relative flex h-[14vh] w-full items-center">
-                  <Avatar className="absolute left-0 top-0 h-[9vh] w-[9vh] rounded-full bg-[#E7E7E7]">
-                    <AvatarImage src={item.avatar} />
-                    <AvatarFallback>{item.name}</AvatarFallback>
-                  </Avatar>
-                  <p className="absolute left-[30%] top-0 text-[2vh] font-semibold md:left-[25%] lg:left-[22%] xl:left-[15%]">
-                    {item.name}
-                  </p>
-                  <p className="absolute left-[30%] top-[4vh] flex items-center gap-[2vh] text-[1.5vh] text-[#0B489B] md:left-[25%] lg:left-[22%] xl:left-[15%]">
-                    研究领域：{item.field}
-                  </p>
-                  <div className="absolute left-[30%] top-[6.5vh] flex w-[50%] flex-col gap-[0.5vh] text-[1.5vh] text-[#0B489B] md:left-[25%] lg:left-[22%] xl:left-[15%] xl:top-[8vh] xl:flex-row xl:items-center">
-                    <p className="flex-1">职称：{item.title}</p>
-                    <p className="flex-1">所属机构：{item.institution}</p>
-                  </div>
-                  <div className="absolute left-[25%] top-[12vh] flex items-center gap-[5vh] text-[1.3vh] text-[#676767] md:left-[22%] lg:left-[20%] xl:left-[13%]">
-                    <p>引用数：{item.citations}</p>
-                    <p>论文数：{item.papers}</p>
-                  </div>
-                  <p className="absolute right-[2%] top-[2vh] flex items-center gap-[1vh] text-[1.3vh] text-[#943C3C]">
-                    {item.isFollowed ? <IoHeart /> : <IoHeartOutline />}
-                    关注人数：{item.followers}
-                  </p>
+  const TABS = [
+    {
+      label: '关注学者',
+      children: (
+        <>
+          {SCHOLAR_ITEMS.map((item) => (
+            <div
+              key={item.id}
+              className="relative flex w-full flex-col items-center"
+            >
+              <div className="relative flex h-[14vh] w-full items-center">
+                <Avatar className="absolute left-0 top-0 h-[9vh] w-[9vh] rounded-full bg-[#E7E7E7]">
+                  <AvatarImage src={item.avatar} />
+                  <AvatarFallback>{item.name}</AvatarFallback>
+                </Avatar>
+                <p className="absolute left-[30%] top-0 text-[2vh] font-semibold md:left-[25%] lg:left-[22%] xl:left-[15%]">
+                  {item.name}
+                </p>
+                <p className="absolute left-[30%] top-[4vh] flex items-center gap-[2vh] text-[1.5vh] text-[#0B489B] md:left-[25%] lg:left-[22%] xl:left-[15%]">
+                  研究领域：{item.field}
+                </p>
+                <div className="absolute left-[30%] top-[6.5vh] flex w-[50%] flex-col gap-[0.5vh] text-[1.5vh] text-[#0B489B] md:left-[25%] lg:left-[22%] xl:left-[15%] xl:top-[8vh] xl:flex-row xl:items-center">
+                  <p className="flex-1">职称：{item.title}</p>
+                  <p className="flex-1">所属机构：{item.institution}</p>
                 </div>
-                <Breakline className="w-[90%] border-t-2" />
+                <div className="absolute left-[25%] top-[12vh] flex items-center gap-[5vh] text-[1.3vh] text-[#676767] md:left-[22%] lg:left-[20%] xl:left-[13%]">
+                  <p>引用数：{item.citations}</p>
+                  <p>论文数：{item.papers}</p>
+                </div>
+                <p className="absolute right-[2%] top-[2vh] flex cursor-pointer items-center gap-[1vh] text-[1.3vh] text-[#943C3C]">
+                  {item.isFollowed ? <IoHeart /> : <IoHeartOutline />}
+                  关注人数：{item.followers}
+                </p>
               </div>
-            ))}
-          </>
-        )}
-      </div>
-    </div>
-  );
+              <Breakline className="w-[90%] border-t-2" />
+            </div>
+          ))}
+        </>
+      ),
+    },
+    {
+      label: '论文收藏',
+      children: <></>,
+    },
+  ];
+
+  return <Tabs tabs={TABS} />;
 };
 
 export default Follow;
