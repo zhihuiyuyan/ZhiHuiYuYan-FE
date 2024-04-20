ccc2a692d16f1ba3719680599915b6ef3c8b5a73
墨綠BG <s5460703@gmail.com>
2024-04-20 20:10:53 +0800

    ✨ feat：优化路由分流

--- src/app/profile/history/page.tsx
+++ src/app/profile/history/page.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next';
 
+import Profile from '@/modules/profile';
+
 const Page: NextPage = () => {
   return (
     <>
-      history page
+      <Profile route="history" />
     </>
   );
 };
--- src/modules/profile/components/Profile.tsx
+++ src/modules/profile/components/Profile.tsx
@@ -1,24 +1,41 @@
 import BreaklineDashed from '@/common/components/elements/BreaklineDashed';
-
 import Experience from './Experience';
 import Follow from './Follow';
 import Information from './Information';
 
 interface ProfileProps {
-  route: 'profile' | 'follow';
+  route: 'profile' | 'follow' | 'history';
 }
 
 const Profile: React.FC<ProfileProps> = ({ route }) => {
+  const commonStyles = "relative flex h-[88vh] w-[60%] min-w-[50vh] flex-col items-center rounded-[3vh] bg-white";
+  const sharedContent = (
+    <div className={commonStyles}>
+      <Information />
+      <BreaklineDashed className="relative w-[85%] border-t-2 border-[#B9B9B9]" />
+    </div>
+  );
+
   return (
-    <>
-      <div className="flex h-[92vh] w-full items-center justify-center bg-[#F9FAFC]">
-        <div className="relative flex h-[88vh] w-[60%] min-w-[50vh] flex-col items-center rounded-[3vh] bg-white">
-          <Information />
-          <BreaklineDashed className="relative w-[85%] border-t-2 border-[#B9B9B9]" />
-          {route === 'profile' ? <Experience /> : <Follow />}
+    <div className="flex h-[92vh] w-full items-center justify-center bg-[#F9FAFC]">
+      {route === 'history' && (
+        <div className={commonStyles}>
+          <>123</>
         </div>
-      </div>
-    </>
+      )}
+      {route === 'profile' && (
+        <div className={commonStyles}>
+          {sharedContent}
+          <Experience />
+        </div>
+      )}
+      {route === 'follow' && (
+        <div className={commonStyles}>
+          {sharedContent}
+          <Follow />
+        </div>
+      )}
+    </div>
   );
 };
 
