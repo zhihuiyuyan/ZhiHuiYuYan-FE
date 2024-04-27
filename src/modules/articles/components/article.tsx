5a9b253acad6e3495d15d4e789ecd6b4ad4a0806
墨綠BG <s5460703@gmail.com>
2024-04-27 21:51:40 +0800

    🌈 style：代码格式化

--- src/modules/articles/components/article.tsx
+++ src/modules/articles/components/article.tsx
@@ -1,6 +1,6 @@
 'use client';
+
 import { AnimatePresence, motion } from 'framer-motion';
-import React from 'react';
 
 const ArticlePage: React.FC<{ article: string }> = ({ article }) => {
   return (
