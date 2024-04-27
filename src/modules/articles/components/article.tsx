6e5e4176033a17bae7c14a6b94ca9e2de6894d4c
konodioda727 <kaiqingliu6@gmail.com>
2024-04-27 21:07:05 +0800

    ✨ Add articleInfo page
    add articleInfo page

---
+++ src/modules/articles/components/article.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const ArticlePage: React.FC<{article: string}> = ({article}) => {
+  return (
+    <AnimatePresence mode='wait'>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.5 }}>
+        <div>{article}</div>
+      </motion.div>
+    </AnimatePresence>
+
+  )
+}
+
+export default ArticlePage
