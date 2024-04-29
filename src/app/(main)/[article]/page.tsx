390bd4177daadf0adfd5713e205454169a1e186d
墨綠BG <s5460703@gmail.com>
2024-04-29 21:17:01 +0800

    🔨 refactor：使用hook代替传参

--- src/app/(main)/[article]/page.tsx
+++ src/app/(main)/[article]/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import { NextPage } from 'next';
+import { useParams } from 'next/navigation';
+
 import ArticlePage from '@/modules/articles';
 
-const Article = ({ params }: { params: { article: string } }) => {
+const Article: NextPage = () => {
+  const params = useParams<{ article: string }>();
+
   return (
     <>
       <ArticlePage article={params.article}></ArticlePage>
